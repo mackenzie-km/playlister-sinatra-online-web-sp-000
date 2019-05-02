39f6e6d37b14b6e55bc73bdcdd1f478a19c7c631
Mackenzie Moore <macmoore@ucdavis.edu>
2019-05-02 17:52:06 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,6 +14,11 @@ class SongsController < ApplicationController
     erb :'songs/new'
   end
 
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+
   post '/songs' do
     slug_name = params[:artists][:name].gsub(" ", "-").downcase
     artist = Artist.find_by_slug(slug_name)
@@ -38,39 +43,35 @@ class SongsController < ApplicationController
     end
   end
 
-  get "/songs/:slug/edit" do
+  get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/edit'
   end
 
-  patch "/songs/:slug" do
-
-    artist_slug = params[:artists][:name].gsub(" ", "-").downcase
-    artist = Artist.find_by_slug(artist_slug)
-    if !artist
-      artist = Artist.create(params[:artists])
-    end
-
+  patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    @song.artist_id = artist.id
-    @song.save
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    # artist = Artist.find_by_slug(params[:artists][:name].gsub(" ", "-").downcase)
+    # if !artist
+    #   artist = Artist.create(params[:artists])
+    # end
+    # = artist
 
-    # if params[:genres] != @song.genres
-    # params[:genres].each do |genre|
-    #   new_line_item = SongGenre.create(genre_id: genre.to_i, song_id: @song.id)
+    # if (params[:genres] != nil) && (params[:genres] != @song.genres.collect {|genre| genre.id.to_s})
+    #     @song.genres.clear!
+    #   params[:genres].each do |genre|
+    #     @song.genres << Genre.find(genre)
+    #     @song.save
+    #   end
     # end
 
-    if @song.save
-      flash[:message] = "Successfully updated song."
-      redirect to "/songs/#{params[:slug]}"
-    else
-      flash[:message] = "Error - try again."
-      redirect to "/songs/#{params[:slug]}/edit"
-    end
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect("/songs/#{@song.slug}")
   end
 
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    erb :'songs/show'
-  end
+
 end
